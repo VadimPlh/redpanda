d87bc394eeccbb3702a60199d432416556123b91
Vadim <vadim@vectorized.io>
2021-09-28 13:51:55 +0300

    add start and stop cluster

---
+++ coproc-test/start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+IPs='127.0.0.1:9092,127.0.0.1:9093,127.0.0.1:9094';
+
+NODE_PATH='../../../build/node/output/modules/rpc/service.js'
+
+cd ./cluster
+
+
+# Start redpanda cluster
+for dir in *
+do
+    cd $dir
+    sh prepare.sh
+    cd ..
+done
+
+for dir in *
+do
+    cd $dir
+    rm -rf log.txt
+    touch log.txt
+    sh start.sh &> log.txt  &
+    count="0"
+    while true
+    do
+        count=`grep "Successfully started Redpanda" log.txt -c`
+        if [ "$count" == "1" ]
+        then
+            break;
+        fi
+    done
+    rm -rf node_log.txt
+    node $NODE_PATH redpanda.yaml &> node_log.txt &
+    cd ..
+done
---
+++ coproc-test/stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pids=`ps aux | grep redpanda | awk '{print $2}'`
+sudo kill -9 $pids
