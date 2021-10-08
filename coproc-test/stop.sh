416fd837f4091623d090e0e534309d24178a6105
Vadim <vadim@vectorized.io>
2021-10-08 16:54:30 +0300

    fix

--- coproc-test/stop.sh
+++ coproc-test/stop.sh
@@ -6,3 +6,13 @@ pids=`ps aux | grep perf-test | awk '{print $2}'`
 sudo kill -9 $pids
 pids=`ps aux | grep kafka | awk '{print $2}'`
 sudo kill -9 $pids
+
+. ./config.sh
+
+RPK_PATH="../vbuild/go/linux/bin/rpk"
+
+for (( i=0; i < ${COPROC_COUNT}; i++ ))
+do
+    $RPK_PATH wasm remove  "one_to_one_${i}"  --brokers ${BROKERS}
+    $RPK_PATH topic delete one_to_one_${i}  --brokers ${BROKERS}
+done
